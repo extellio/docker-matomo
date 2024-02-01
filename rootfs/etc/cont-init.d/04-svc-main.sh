a990267bfb0205685fc613d61601be02a55724df
Carl Grundberg <carl.grundberg@menmo.se>
2024-02-01 09:40:17 +0100

    Dont upgrade or set config at startup

--- rootfs/etc/cont-init.d/04-svc-main.sh
+++ rootfs/etc/cont-init.d/04-svc-main.sh
@@ -7,21 +7,21 @@ if [ "$SIDECAR_CRON" = "1" ]; then
   exit 0
 fi
 
-LOG_LEVEL=${LOG_LEVEL:-WARN}
+# LOG_LEVEL=${LOG_LEVEL:-WARN}
 
 # Check if already installed
-if [ -f "/data/config/config.ini.php" ]; then
-  echo "Setting Matomo log level to $LOG_LEVEL..."
-  console config:set --section=log --key=log_level --value="$LOG_LEVEL"
+# if [ -f "/data/config/config.ini.php" ]; then
+#   echo "Setting Matomo log level to $LOG_LEVEL..."
+#   console config:set --section=log --key=log_level --value="$LOG_LEVEL"
 
-  echo "Upgrading and setting Matomo configuration..."
-  console core:update --yes --no-interaction
-  console config:set --section=General --key=minimum_memory_limit --value=-1
-else
-  echo ">>"
-  echo ">> Open your browser to install Matomo through the wizard"
-  echo ">>"
-fi
+#   echo "Upgrading and setting Matomo configuration..."
+#   console core:update --yes --no-interaction
+#   console config:set --section=General --key=minimum_memory_limit --value=-1
+# else
+#   echo ">>"
+#   echo ">> Open your browser to install Matomo through the wizard"
+#   echo ">>"
+# fi
 
 mkdir -p /etc/services.d/nginx
 cat > /etc/services.d/nginx/run <<EOL
