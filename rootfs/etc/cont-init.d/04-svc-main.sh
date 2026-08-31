191265393fdd420f8abf3bdf7f6d2d01d377addb
Carl Grundberg <carl.grundberg@menmo.se>
2026-08-31 17:09:45 +0200

    Update Matomo version to 5.13.0 and comment out configuration update logic in init script

--- rootfs/etc/cont-init.d/04-svc-main.sh
+++ rootfs/etc/cont-init.d/04-svc-main.sh
@@ -10,18 +10,18 @@ fi
 LOG_LEVEL=${LOG_LEVEL:-WARN}
 
 # Check if already installed
-if [ -f "/data/config/config.ini.php" ] && grep -q '^\[database\]' "/data/config/config.ini.php"; then
-  echo "Setting Matomo log level to $LOG_LEVEL..."
-  console config:set --section=log --key=log_level --value="$LOG_LEVEL"
+# if [ -f "/data/config/config.ini.php" ] && grep -q '^\[database\]' "/data/config/config.ini.php"; then
+#   echo "Setting Matomo log level to $LOG_LEVEL..."
+#   console config:set --section=log --key=log_level --value="$LOG_LEVEL"
 
-  echo "Upgrading and setting Matomo configuration..."
-  console core:update --yes --no-interaction
-  console config:set --section=General --key=minimum_memory_limit --value=-1
-else
-  echo ">>"
-  echo ">> Open your browser to install Matomo through the wizard"
-  echo ">>"
-fi
+#   echo "Upgrading and setting Matomo configuration..."
+#   console core:update --yes --no-interaction
+#   console config:set --section=General --key=minimum_memory_limit --value=-1
+# else
+#   echo ">>"
+#   echo ">> Open your browser to install Matomo through the wizard"
+#   echo ">>"
+# fi
 
 mkdir -p /etc/services.d/nginx
 cat > /etc/services.d/nginx/run <<EOL
